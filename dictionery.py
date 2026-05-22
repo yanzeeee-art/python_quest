d737fb9bd2aa1102644c15638838f88b67704030
anjee tamang <anjeetamang08.com>
2026-05-22 08:23:59 +0545

    help

---
+++ dictionery.py
@@ -0,0 +1,17 @@
+student = {
+    "name":"pretty",
+    "age": "20",
+    "class":"10",
+    "dob":"2005-01-18"
+}
+print(student)
+print(student["age"])
+student["school"]=" ABC school"
+student["age"]=19
+print(student)
+
+del student ["class"]
+print(student)
+
+
+
