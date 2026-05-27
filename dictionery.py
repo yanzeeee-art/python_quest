4d8d2ee021b4cdbfb1132cdb36e15ef3dcdb3ecf
anjee tamang <anjeetamang08.com>
2026-05-27 08:34:56 +0545

    pretty

--- dictionery.py
+++ dictionery.py
@@ -17,4 +17,19 @@ print(student.keys())
 
 print(student.items())
 
+#to convert dict into tuple
+my_tuple =tuple(student.items())
+print(my_tuple)
+
+#to convert tuple into dict.
+my_tuple =(('name' ,'ram'),('age','20'), ('class','10'))
+my_dict =dict(my_tuple)
+print(my_dict)
+
+
+#to convert dict into list
+my_list =list(student.items())
+print(my_list)
+
+
 
---
+++ sets.py
@@ -0,0 +1,18 @@
+myset = {"apple", "banana", "cherry"}
+print(myset)
+print(type(myset))
+
+numbers = {1, 2, 3, 4 ,5}
+print (numbers)
+
+colors ={"red","blue","green"}
+print (colors)
+
+#to add an item to a set 
+fruits ={"apple", "banana"}
+fruits.add("mango")
+print(fruits)
+#update
+fruits ={"apple", "banana"}
+fruits.update({"litchi", "papaya"})
+print(fruits)
