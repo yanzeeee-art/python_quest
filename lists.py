bdcee7a8df7298e363edca649b34d7b95df5811d
anjee tamang <anjeetamang08.com>
2026-05-27 07:45:10 +0545

    yayy

--- lists.py
+++ lists.py
@@ -1,11 +1,11 @@
-lists =list(("hello", "10", "school", "apple", "20", "happy"))
+lists =list["hello", "10", "school", "apple", "20", "happy"]
 print(lists[0])
 print(lists[0:3])
 print(lists[::2])
 print(lists[::-2])
 
 
- 
+ #to convert lists into tuple
 my_tuple =tuple(lists)
 print(my_tuple)
 
--- tuple.py
+++ tuple.py
@@ -1,7 +1,11 @@
-mytuple = ("apple", "banana", "cherry", "10", "20","30")
+mytuple = ( "banana","apple", "cherry",  "20","10","30")
 print(mytuple)
 print(len(mytuple))
 print(mytuple[2])
 print(mytuple[1:5])
 print(mytuple[::3])
 print(mytuple[::-2])
+
+#to convert tuple into lists
+mylists =list(mytuple)
+print(mylists)
